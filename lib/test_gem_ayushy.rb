1bfdd05e59eb972fde961c040c5db17ce9d6dc3c
shubham p <sprajapat.tout@gmail.com>
2022-10-17 19:09:23 +0530

    initial commit

---
+++ lib/test_gem_ayushy.rb
@@ -0,0 +1,9 @@
+require "test_gem_ayushy/version"
+
+module TestGemAyushy
+  class Error < StandardError; end
+  
+  def hi
+    'Hi, I am Ayushy, Testing gem making flow'
+  end
+end
---
+++ lib/test_gem_ayushy/version.rb
@@ -0,0 +1,3 @@
+module TestGemAyushy
+  VERSION = "0.1.0"
+end
